b9ab5ed6a33a16be31f37f671ecb273f108dfd1e
rin <normierandomzz@gmail.com>
2024-09-09 19:26:23 +0900

    initial commit

---
+++ hello-dotnet/hello-dotnet.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace hello_dotnet.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
